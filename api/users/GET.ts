62d39b5b720dd89b64dce48233fc2976990df696
naoki yoshizawa <fonces.xyz@gmail.com>
2021-04-12 15:53:24 +0900

    update envs

--- api/users/GET.ts
+++ api/users/GET.ts
@@ -7,7 +7,7 @@ export type UserRow = {
 
 type Columns = 'teamId' | 'teamName' | 'userId' | 'userName'
 
-const getUsers = () => fetch(process.env.ssEndPoint!)
+const getUsers = () => fetch(process.env.NEXT_PUBLIC_SS_ENDPOINT!)
   .then(res => res.json())
   .then(res => (res as Record<Columns, string>[]).map(({ teamId, teamName, userId, userName }) => ({
     teamId,
--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 const path = require('path')
 
-if (!process.env.GITHUB_PAGES) {
-  require('dotenv').config()
-}
-
 module.exports = {
   assetPrefix: process.env.GITHUB_PAGES ? '/tm-frontend' : '',
-  env: {
-    ssEndPoint: process.env.SS_ENDPOINT,
-  },
   webpack (config, options) {
     config.resolve.alias['@'] = path.join(__dirname, './')
     return config
