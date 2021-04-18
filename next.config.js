d9fe9339f0b10683924246371197e79e9bfd8a2c
fonces <fonces.xyz@gmail.com>
2021-04-18 22:43:43 +0900

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
+const withPWA = require('next-pwa')
 const path = require('path')
 
-module.exports = {
-  assetPrefix: process.env.GITHUB_PAGES ? '/tm-frontend' : '',
+const BASE_PATH = process.env.GITHUB_PAGES ? '/tm-frontend' : ''
+
+module.exports = withPWA({
+  assetPrefix: BASE_PATH,
+  env: {
+    basePath: BASE_PATH,
+  },
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+  },
   webpack (config, options) {
     config.resolve.alias['@'] = path.join(__dirname, './')
     return config
   },
-}
+})
