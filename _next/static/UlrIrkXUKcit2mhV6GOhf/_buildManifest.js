92c02bad4cc9368b53c09bb890b6960baf8056b9
fonces <fonces@users.noreply.github.com>
2021-11-11 09:28:50 +0000

    deploy: 5f97a4990fcc7486d5d374534d521536a30c8b0c

---
+++ _next/static/UlrIrkXUKcit2mhV6GOhf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/30-f92324d1246b9f256037.js","static/chunks/pages/index-ab4f6c9061ee04e35ef3.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
